96444b9d0e5ee6113de23a4a7605427d66956cb7
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2018-08-20 07:28:33 -0400

    Add url-loader and file-loader to build, with logo example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import image from './components/Image';
 import { showChart } from './hike-chart';
 
+import twoHikersLogo from './assets/images/two-hikers.svg';
+
 console.log('It really works!');
+console.log(twoHikersLogo);
+
+const siteHeader = document.getElementById('siteheader');
+const siteLogo = image(twoHikersLogo);
+siteHeader.appendChild(siteLogo);
 
 showChart();
